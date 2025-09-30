d7f10f1233dd1da1f7057f41f2d6a3c8b44d8a7f
Dhara Pandya <dharapandya2020@gmail.com>
2025-09-30 18:36:50 +0530

    feat(subscription): Implement Subscription Service with USDC Payment and Ledger Standardization

--- contracts/assetsup/src/lib.rs
+++ contracts/assetsup/src/lib.rs
@@ -1,11 +1,15 @@
 #![no_std]
-use soroban_sdk::{Address, Env, contract, contractimpl, contracttype};
+use soroban_sdk::{Address, Env, BytesN,contract, contractimpl};
 
-#[contracttype]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum DataKey {
-    Admin,
-}
+//Import types and modules
+mod errors;
+mod types;
+mod subscription;
+
+//Import types and contract logic
+use crate::types::DataKey;
+use crate::subscription::{SubscriptionContract, SubscriptionService};
+
 
 #[contract]
 pub struct AssetUpContract;
@@ -24,6 +28,29 @@ impl AssetUpContract {
     pub fn get_admin(env: Env) -> Address {
         env.storage().persistent().get(&DataKey::Admin).unwrap()
     }
-}
 
-mod tests;
+    //creates a new subscription
+    pub fn create_subscription(
+        env: Env,
+        id: BytesN<32>,
+        user: Address,
+        plan: crate::types::PlanType,
+        payment_token: Address,
+        duration_days: u32,
+    ) ->Result<crate::types::Subscription, errors::ContractError>{
+        SubscriptionService::create_subscription(env, id, user, plan, payment_token, duration_days)
+    } 
+    /// Cancels an active subscription.
+    pub fn cancel_subscription(env: Env, id:soroban_sdk::BytesN<32>) -> Result<crate::types::Subscription, errors::ContractError>{
+        SubscriptionService::cancel_subscription(env, id)
+    }
+    /// Retrieves subscription details.
+    pub fn get_subscription(env: Env, id:soroban_sdk::BytesN<32>) -> Result<crate::types::Subscription, errors::ContractError>{
+        SubscriptionService::get_subscription(env, id)
+    }
+}
+//export contract name for testing in other modules
+#[cfg(test)]
+mod tests{
+    
+}
