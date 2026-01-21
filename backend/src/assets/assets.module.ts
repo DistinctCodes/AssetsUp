ec920be8abcc9e4af041cf6b8f64b17ee4471db2
Musa Khalid <112591148+Mkalbani@users.noreply.github.com>
2026-01-21 20:37:38 +0000

    feat: implemented asset mgt

---
+++ backend/src/assets/assets.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AssetsService } from './assets.service';
+import { AssetsController } from './assets.controller';
+import { Asset } from './entities/asset.entity';
+// import { AssetCategory } from '../asset-categories/asset-category.entity';
+// import { Department } from '../departments/department.entity';
+// import { User } from '../users/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Asset,
+    //   AssetCategory,
+    //   Department,
+    //   User,
+    ]),
+  ],
+  controllers: [AssetsController],
+  providers: [AssetsService],
+  exports: [AssetsService],
+})
+export class AssetsModule {}
