069bf63e07ad445b8a96126cedfb079d65b2fc05
Annakaee <annakaee@example.com>
2026-06-25 06:06:40 +0100

    chore: add passport-google-oauth20 to package.json to fix CI build

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from "./app.service";
-import { UsersModule } from "./users/users.module";
-import { AuthModule } from "./auth/auth.module";
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
