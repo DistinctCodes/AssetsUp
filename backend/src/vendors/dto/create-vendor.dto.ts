f9c80a06c6f3f29152daff465851b903cfe41f3f
devzakari-sketch <devzakari@gmail.com>
2026-08-25 18:32:29 +0100

    feat(vendors): add CreateVendorDto with class-validator decorators

---
+++ backend/src/vendors/dto/create-vendor.dto.ts
@@ -0,0 +1,41 @@
+import { IsBoolean, IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class CreateVendorDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  code: string;
+
+  @IsOptional()
+  @IsString()
+  contactName?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  website?: string;
+
+  @IsOptional()
+  @IsString()
+  taxId?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+
+  @IsOptional()
+  @IsString()
+  notes?: string;
+}
